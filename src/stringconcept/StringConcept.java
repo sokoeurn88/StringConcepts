440f3bd1925edc3a99679afeee48e763c756f17d
sokoeurn88 <92101403+sokoeurn88@users.noreply.github.com>
2022-09-03 19:24:02 -0500

    string concepts

---
+++ src/stringconcept/StringConcept.java
@@ -0,0 +1,35 @@
+package stringconcept;
+
+public class StringConcept {
+	final static int a = 5;
+
+	public static void main(String[] args) {
+
+// java immutable or final
+		
+		String s1 = "Java";
+		String s2 = "Java";
+		String s3 = "C++";
+		
+		System.out.println("s1");
+		System.out.println("s2");
+		System.out.println("s3");
+		
+//Static in Java
+		int b = 10;
+		System.out.println(b);
+		
+		StringConcept sc = new StringConcept();
+		System.out.println(sc.a);
+		
+		System.out.println(a);
+		
+		System.out.println(a+b);
+		
+//final in Java
+		
+		
+
+	}
+
+}
